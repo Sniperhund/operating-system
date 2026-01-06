6d53ace8fb2de2611ab4aa0a2f5299e1670b910c
Lucas Täkker <lucastakker@gmail.com>
2026-01-06 17:01:20 +0100

    Added timer and keyboard irq handler

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -2,7 +2,22 @@
 #include "panic.h"
 #include "x86/gdt.h"
 #include "x86/idt.h"
+#include "x86/io.h"
+#include "x86/irq.h"
+#include "x86/pic.h"
 #include <stdio.h>
+
+void timer(CPUStatus* status) {
+    printf(".");
+}
+
+void keyboard(CPUStatus* status) {
+    uint8_t scancode = inb(0x60);
+
+    if (scancode & 0x80) return;
+
+    printf("%X", scancode);
+}
 
 extern "C" void kernel_main() {
     Text::setColor(Text::BLACK, Text::LIGHT_BLUE);
@@ -14,8 +29,11 @@ extern "C" void kernel_main() {
 
     Text::puts("GDT switch\n");
 
+    PIC::remap();
     IDT::init();
+    IRQ::registerIRQ(0, timer);
+    IRQ::registerIRQ(1, keyboard);
 
     Text::puts("IDT enabled\n");
-    printf("Number: %d", 12);
+    printf("Number: %d\n", 12);
 }
