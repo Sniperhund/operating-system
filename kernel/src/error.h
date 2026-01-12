6efa5cb1b02135159ea7b343ad4dc25445633272
Lucas Täkker <lucastakker@gmail.com>
2026-01-12 18:14:12 +0100

    Nearly finished RamFS

---
+++ kernel/src/error.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define E_NOENT     1 /* No such file or directory */
+#define E_INVAL     2 /* Invalid or missing argument */
+#define E_NOTDIR    3 /* Not a directory */
+#define E_ISDIR     4 /* Is a directory */
+#define E_NOTUNIQ   5 /* Name not unique */
