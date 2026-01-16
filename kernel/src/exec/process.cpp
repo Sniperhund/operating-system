8748f3ed07aa20892e9c803a66d7f7ef58d85a58
Lucas Täkker <lucastakker@gmail.com>
2026-01-16 18:46:05 +0100

    Removed comments from exec

--- kernel/src/exec/process.cpp
+++ kernel/src/exec/process.cpp
@@ -31,19 +31,16 @@ void exec(const char *cmd, const char *args) {
 
     if (!proc->pd) PANIC("PROCESS", "Page Directory couldn't be allocated");
 
-    // Create a kernel and user stack
-
-    // Heap allocator allocates in higher half, so is always mapped
     proc->kstack = (void*)((uintptr_t)Heap::alloc(KERNEL_STACK_SIZE) + KERNEL_STACK_SIZE);
-    // mmap makes it possible to map and "allocate" in lower half
+
     void* userStackBottom = mmap(proc->pd, (void*)USER_STACK_SPACE, USER_STACK_SIZE, PROT_WRITE);
     proc->stack = (void*)((uintptr_t)userStackBottom + USER_STACK_SIZE);
     proc->ctx.useresp = (uintptr_t)proc->stack;
     proc->ctx.esp = (uintptr_t)proc->kstack;
 
-    // Switch to the page directory given (kernel is automatically mapped)
+
     Paging::switchPD(proc->pd, false);
-    // Load the ELF (maps user pages itself)
+
     inode* file = VFS::open(cmd);
     if (!file) PANIC("PROCESS", "Couldn't open file");
 
@@ -53,12 +50,11 @@ void exec(const char *cmd, const char *args) {
     ELFLoader::loadExecutable(buffer, &proc->ctx.eip);
 
     Heap::free(buffer);
-    // Set state to READY
+
     proc->state = READY;
     proc->ctx.eflags = 0x202;
-    // Add the process to the scheduler
+
     asm volatile("cli");
     Scheduler::addProcess(proc);
     asm volatile("sti");
-    // Notify scheduler that it's ready, when context switching is implemented let the scheduler do that itself
 }
