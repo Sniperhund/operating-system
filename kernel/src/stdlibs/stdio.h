659089fd2d822daca6bc5e760e2822a1847cd8fe
Lucas Täkker <lucastakker@gmail.com>
2026-01-30 14:24:06 +0100

    Added initial Doxygen support

--- kernel/src/stdlibs/stdio.h
+++ kernel/src/stdlibs/stdio.h
@@ -7,14 +7,14 @@ int sprintf(char* s, const char* format, ...);
 
 /**
   * Format a string with the following specifiers:
-  *  d - Signed decimal integer
-  *  u - Unsigned decimal integer
-  *  x - Unsigned hexadecimal integer
-  *  X - Unsigned hexadecimal integer (uppercase)
-  *  b - Unsigned bit integer
-  *  c - Character
-  *  s - String of characters
-  *  p - Pointer address
-  *  % - Write a single '%'
+  * @li d - Signed decimal integer
+  * @li u - Unsigned decimal integer
+  * @li x - Unsigned hexadecimal integer
+  * @li X - Unsigned hexadecimal integer (uppercase)
+  * @li b - Unsigned bit integer
+  * @li c - Character
+  * @li s - String of characters
+  * @li p - Pointer address
+  * @li % - Write a single '%'
   */
 int vsprintf(char* s, const char* format, va_list arg);
